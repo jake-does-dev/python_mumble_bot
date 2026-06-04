2c7747e676e64b3dfba0d832b2ba9d8d2839286f
Jake Allan <jjemail1995@gmail.com>
2026-06-04 17:10:58 +0100

    chore(mumble-ux): improve latency (#55)

--- mumble_bot/python_mumble_bot/__main__.py
+++ mumble_bot/python_mumble_bot/__main__.py
@@ -1,3 +1,10 @@
+import logging
+
 import python_mumble_bot.bot.client as client
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s %(message)s",
+)
 
 client.connect()
